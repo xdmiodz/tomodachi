c8a2276ade383030bf5fcad5ff32e92168c9f49b
Dmitry Odzerikho <dmitry.odzerikho@gmail.com>
2019-08-01 15:02:04 +0300

    tune deps for internal needs

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ import tomodachi.__version__
 
 install_requires = [
     'pycparser>=2.18',
-    'aioamqp>=0.10.0, <0.13.0',
+    'aioamqp>=0.10.0',
     'ujson>=1.35',
     'uvloop>=0.8.1',
-    'aiobotocore>=0.6.0, <0.11.0',
     'tzlocal>=1.4',
     'aiohttp>=3.0.5, <3.6.0',
     'yarl>=1.1.0',
